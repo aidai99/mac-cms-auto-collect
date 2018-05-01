99b31474ec2a94747aa66ea938a60771d18ee188
wktadmin <37318380+wktadmin@users.noreply.github.com>
2018-05-01 18:40:02 +0800

    检验配置是否出错

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import threading
 import userinfo
 from userinfo import UserInfo
 from colects import StartCollect
+from getCookies import GetCookies
 from view import *
 import time
 import os
 softwarestatus = -1
-
 usi = UserInfo()
+    
 def checkVersion():
     global softwarestatus
     softwarestatus = UserInfo().softwarestatus    
@@ -17,6 +18,13 @@ def checkVersion():
 def run():
     if os.path.exists(userinfo.filePath):
         print("检测到登陆文件")
+        # 检测登录是否出错:
+        try:
+            GetCookies().checkLogin()
+        except:
+            print('配置出错,  已经为你重置')
+            os.remove(userinfo.filePath)
+            return run()
         startnum = startprint()
         if startnum == 0 and softwarestatus == -1:
             choose = IndexList()
@@ -44,10 +52,11 @@ def run():
         
 
 
-if __name__ == '__main__':
+    
+
+if __name__ == '__main__':    
     run_thread = threading.Thread(target=run)
     check_thread = threading.Thread(target=checkVersion)
     run_thread.start()
     time.sleep(10)
     check_thread.start()
-    
